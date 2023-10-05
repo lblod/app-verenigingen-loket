f61dd3cc361757e7a8de5030116a1372b60849ca
oscartison <oscartison@me.com>
2023-10-05 09:15:26 +0000

    Add associations

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -16,6 +16,18 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/organizations/"
   end
 
+  match "/contact-points/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/contact-points/"
+  end
+
+  match "/sites/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/sites/"
+  end
+
+  match "/addresses/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/addresses/"
+  end
+
   match "/administrative-unit-classification-codes/*path", %{ accept: [:json], layer: :api} do
     Proxy.forward conn, path, "http://cache/administrative-unit-classification-codes/"
   end
@@ -36,10 +48,15 @@ defmodule Dispatcher do
     forward conn, path, "http://cache/users/"
   end
 
+  match "/associations/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/associations/"
+  end
+
 
   match "/*_", %{accept: [:any], layer: :not_found} do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
 
 end
+
 
