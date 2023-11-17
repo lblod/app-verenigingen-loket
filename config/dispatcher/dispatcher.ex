6619190015f87075be026de74a0773809c7ca278
oscartison <oscartison@me.com>
2023-11-17 10:29:16 +0000

    Add types and target audiences

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -80,6 +80,11 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/memberships/"
   end
 
+  match "/concept-schemes/*path", %{ accept: [:json], layer: :api} do
+    Proxy.forward conn, path, "http://cache/concept-schemes/"
+  end
+
+
   match "/postal-codes/*path", %{ accept: [:any], layer: :api} do
     Proxy.forward conn, path, "http://cache/postalCodes/"
   end
@@ -89,24 +94,33 @@ defmodule Dispatcher do
   end
 
   match "/change-events/*path", %{ accept: [:any], layer: :api} do
-    Proxy.forward conn, path, "http://resource/change-events/"
+    Proxy.forward conn, path, "http://cache/change-events/"
   end
 
-  match "/assets/*path", %{ layer: :api } do
-    Proxy.forward conn, path, "http://frontend/assets/"
+  match "/concepts/*path", %{ accept: [:any], layer: :api} do
+    Proxy.forward conn, path, "http://cache/concepts/"
   end
 
-  match "/@appuniversum/*path", %{ layer: :api } do
-    Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  match "/target-audiences/*path", %{ accept: [:any], layer: :api} do
+    Proxy.forward conn, path, "http://cache/target-audiences/"
   end
+  
 
-  match "/*path", %{ accept: [:html], layer: :api } do
-    Proxy.forward conn, [], "http://frontend/index.html"
-  end
+  # match "/assets/*path", %{ layer: :api } do
+  #   Proxy.forward conn, path, "http://frontend/assets/"
+  # end
 
-  match "/*_path", %{ layer: :frontend } do
-    Proxy.forward conn, [], "http://frontend/index.html"
-  end
+  # match "/@appuniversum/*path", %{ layer: :api } do
+  #   Proxy.forward conn, path, "http://frontend/@appuniversum/"
+  # end
+
+  # match "/*path", %{ accept: [:html], layer: :api } do
+  #   Proxy.forward conn, [], "http://frontend/index.html"
+  # end
+
+  # match "/*_path", %{ layer: :frontend } do
+  #   Proxy.forward conn, [], "http://frontend/index.html"
+  # end
 
 
   match "/*_", %{accept: [:any], layer: :not_found} do
