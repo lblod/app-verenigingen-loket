83c877bbc56c06ca6ea37ad50a740aeb00491d3e
Andres Vergauwen <andres.vergauwen@hotmail.com>
2024-06-13 13:05:19 +0000

    Fix util query

--- config/consumer/harvester/util.js
+++ config/consumer/harvester/util.js
@@ -209,21 +209,47 @@ async function moveToOrgGraph (muUpdate, endpoint) {
                   adres:postnaam ?name .
 
     { graph  <http://mu.semte.ch/graphs/ingest> {
-          ?association
-                  mu:uuid ?Auuid ;
-                  skos:prefLabel ?alabel ;
-                  dcterms:description ?adescription ;
-                  adms:identifier ?aidentifier ;
-                  reorg:orgStatus ?astatus ;
-                  reorg:orgActivity ?activity ;
-                  ns:korteNaam ?aKorteNaam ;
-                  org:hasSite ?site ;
-                  verenigingen_ext:doelgroep ?doelgroep ;
-                  adms:identifier ?identifier ;
-                  org:classification ?classificatie ;
-                  pav:createdOn ?createdOn ;
-                  org:hasPrimarySite ?primarySite;
-                  org:hasMembership ?membership .
+
+    ?association mu:uuid ?Auuid ;
+       org:hasPrimarySite ?primarySite .
+
+    OPTIONAL {
+      ?association skos:prefLabel ?alabel .
+    }
+    OPTIONAL {
+      ?association dcterms:description ?adescription .
+    }
+    OPTIONAL {
+      ?association adms:identifier ?aidentifier .
+    }
+    OPTIONAL {
+      ?association reorg:orgStatus ?astatus .
+    }
+    OPTIONAL {
+      ?association reorg:orgActivity ?activity .
+    }
+    OPTIONAL {
+      ?association ns:korteNaam ?aKorteNaam .
+    }
+    OPTIONAL {
+      ?association org:hasSite ?site .
+    }
+    OPTIONAL {
+      ?association verenigingen_ext:doelgroep ?doelgroep .
+    }
+    OPTIONAL {
+      ?association adms:identifier ?identifier .
+    }
+    OPTIONAL {
+      ?association org:classification ?classificatie .
+    }
+    OPTIONAL {
+      ?association pav:createdOn ?createdOn .
+    }
+
+    OPTIONAL {
+      ?association org:hasMembership ?membership .
+    }
 
                   OPTIONAL {
                     ?association schema:contactPoint ?contactPointPhone .
@@ -319,144 +345,128 @@ async function moveToOrgGraph (muUpdate, endpoint) {
 ${PREFIXES}
 INSERT {
   GRAPH ?g {
-
-    ?association
-          a <https://data.vlaanderen.be/ns/FeitelijkeVerenigingen#FeitelijkeVereniging>  ;
-          org:hasSite ?site ;
-          org:hasPrimarySite ?primarySite .
-
-
-   ?primarySite
-         a org:Site ;
-         organisatie:bestaatUit ?adres ;
-         dcterms:description ?pDes ;
-         ere:vestigingstype ?pSiteType ;
-         mu:uuid ?Puuid .
-
-  ?pSiteType a code:TypeVestiging ;
-             mu:uuid ?pSiteTypeUuid ;
-             skos:prefLabel ?pSiteTypeName .
-
-  ?site
-         a org:Site ;
-         organisatie:bestaatUit ?sadres ;
-         dcterms:description ?sDes ;
-         ere:vestigingstype ?sSiteType ;
-         mu:uuid ?Suuid .
-
-  ?sSiteType a code:TypeVestiging ;
-         mu:uuid ?sSiteTypeUuid ;
-         skos:prefLabel ?sSiteTypeName .
-
-  ?adres a <http://www.w3.org/ns/locn#Address> ;
-         mu:uuid ?adUuid ;
-         locn:postCode ?code ;
-         locn:thoroughfare ?adStraatnaam ;
-         adres:Adresvoorstelling.busnummer ?adBusnummer ;
-         adres:Adresvoorstelling.huisnummer ?adHuisnummer ;
-         adres:land ?adLand ;
-         locn:fullAddress ?fullAddress ;
-         adres:gemeentenaam ?adGemeente .
-
-  ?sadres a <http://www.w3.org/ns/locn#Address> ;
-         mu:uuid ?sadUuid ;
-         locn:postCode ?sadPostcode ;
-         locn:thoroughfare ?sadStraatnaam ;
-         adres:Adresvoorstelling.busnummer ?sadBusnummer ;
-         adres:Adresvoorstelling.huisnummer ?sadHuisnummer ;
-         adres:land ?sadLand ;
-         locn:fullAddress ?sadFullAddress ;
-         adres:gemeentenaam ?sadGemeente .
+    ?association a <https://data.vlaanderen.be/ns/FeitelijkeVerenigingen#FeitelijkeVereniging> ;
+                 org:hasSite ?site ;
+                 org:hasPrimarySite ?primarySite .
+    ?primarySite a org:Site ;
+                 organisatie:bestaatUit ?adres ;
+                 dcterms:description ?pDes ;
+                 ere:vestigingstype ?pSiteType ;
+                 mu:uuid ?Puuid .
+    ?pSiteType a code:TypeVestiging ;
+               mu:uuid ?pSiteTypeUuid ;
+               skos:prefLabel ?pSiteTypeName .
+    ?site a org:Site ;
+          organisatie:bestaatUit ?sadres ;
+          dcterms:description ?sDes ;
+          ere:vestigingstype ?sSiteType ;
+          mu:uuid ?Suuid .
+    ?sSiteType a code:TypeVestiging ;
+               mu:uuid ?sSiteTypeUuid ;
+               skos:prefLabel ?sSiteTypeName .
+    ?adres a <http://www.w3.org/ns/locn#Address> ;
+           mu:uuid ?adUuid ;
+           locn:postCode ?code ;
+           locn:thoroughfare ?adStraatnaam ;
+           adres:Adresvoorstelling.busnummer ?adBusnummer ;
+           adres:Adresvoorstelling.huisnummer ?adHuisnummer ;
+           adres:land ?adLand ;
+           locn:fullAddress ?fullAddress ;
+           adres:gemeentenaam ?adGemeente .
+    ?sadres a <http://www.w3.org/ns/locn#Address> ;
+            mu:uuid ?sadUuid ;
+            locn:postCode ?sadPostcode ;
+            locn:thoroughfare ?sadStraatnaam ;
+            adres:Adresvoorstelling.busnummer ?sadBusnummer ;
+            adres:Adresvoorstelling.huisnummer ?sadHuisnummer ;
+            adres:land ?sadLand ;
+            locn:fullAddress ?sadFullAddress ;
+            adres:gemeentenaam ?sadGemeente .
   }
 }
-
-
 WHERE {
-
-{graph <http://mu.semte.ch/graphs/public> {
-      ?bestuurseenheid
-          mu:uuid ?adminUnitUuid;
-          org:classification/skos:prefLabel "Gemeente" ;
-          besluit:werkingsgebied ?werkingsgebied .
-
-  ?werkingsgebied
-    rdf:type	ns1:Location ;
-    rdfs:label	?label ;
-    ns3:werkingsgebiedNiveau ?gemeente .
-
-}}
-
-  ?postInfo
-          geo:sfWithin ?werkingsgebied;
-          a adres:Postinfo ;
-          adres:postcode ?code ;
-          adres:postnaam ?name .
-
-{ graph  <http://mu.semte.ch/graphs/ingest> {
-  ?association
-          org:hasSite ?site ;
-          org:hasPrimarySite ?primarySite .
-
-
-  ?primarySite
-        organisatie:bestaatUit ?adres ;
-        mu:uuid ?Puuid .
-        OPTIONAL { ?primarySite dcterms:description ?pDes }
-        OPTIONAL { ?primarySite ere:vestigingstype ?pSiteType }
-
-  OPTIONAL {
-  ?pSiteType a code:TypeVestiging ;
-         mu:uuid ?pSiteTypeUuid ;
-         skos:prefLabel ?pSiteTypeName .
+  {
+    GRAPH <http://mu.semte.ch/graphs/public> {
+      ?bestuurseenheid mu:uuid ?adminUnitUuid ;
+                       org:classification/skos:prefLabel "Gemeente" ;
+                       besluit:werkingsgebied ?werkingsgebied .
+      ?werkingsgebied rdf:type ns1:Location ;
+                      rdfs:label ?label ;
+                      ns3:werkingsgebiedNiveau ?gemeente .
+    }
   }
-  ?site
-        organisatie:bestaatUit ?sadres ;
-        mu:uuid ?Suuid .
-        OPTIONAL { ?primarySite dcterms:description ?sDes }
-        OPTIONAL { ?primarySite ere:vestigingstype ?sSiteType }
-  OPTIONAL {
-  ?sSiteType a code:TypeVestiging ;
-         mu:uuid ?sSiteTypeUuid ;
-         skos:prefLabel ?sSiteTypeName .
-  }
-  ?adres a <http://www.w3.org/ns/locn#Address> ;
-        mu:uuid ?adUuid ;
-        locn:postCode ?code ;
-        locn:thoroughfare ?adStraatnaam ;
-        adres:land ?adLand ;
-        adres:gemeentenaam ?adGemeente .
+  ?postInfo geo:sfWithin ?werkingsgebied ;
+            a adres:Postinfo ;
+            adres:postcode ?code ;
+            adres:postnaam ?name .
+  {
+    GRAPH <http://mu.semte.ch/graphs/ingest> {
+      ?association a <https://data.vlaanderen.be/ns/FeitelijkeVerenigingen#FeitelijkeVereniging> .
+      ?association org:hasPrimarySite ?primarySite .
+      OPTIONAL {
+        ?association org:hasSite ?site .
+      }
+      ?primarySite organisatie:bestaatUit ?adres ;
+                   mu:uuid ?Puuid .
+      OPTIONAL {
+        ?primarySite dcterms:description ?pDes .
+      }
+      OPTIONAL {
+        ?primarySite ere:vestigingstype ?pSiteType .
+      }
+      OPTIONAL {
+        ?pSiteType a code:TypeVestiging ;
+                   mu:uuid ?pSiteTypeUuid ;
+                   skos:prefLabel ?pSiteTypeName .
+      }
+      OPTIONAL {
+        ?adres a <http://www.w3.org/ns/locn#Address> ;
+               mu:uuid ?adUuid ;
+               locn:postCode ?code ;
+               locn:thoroughfare ?adStraatnaam ;
+               adres:land ?adLand ;
+               adres:gemeentenaam ?adGemeente .
         OPTIONAL {
-        ?adres a <http://www.w3.org/ns/locn#Address> ;
-        adres:Adresvoorstelling.busnummer ?adBusnummer .
+          ?adres adres:Adresvoorstelling.busnummer ?adBusnummer .
         }
-                OPTIONAL {
-        ?adres a <http://www.w3.org/ns/locn#Address> ;
-        adres:Adresvoorstelling.huisnummer ?adHuisnummer .
+        OPTIONAL {
+          ?adres adres:Adresvoorstelling.huisnummer ?adHuisnummer .
         }
-
-  BIND(CONCAT(?adStraatnaam, " ", ?adHuisnummer, " ", ?adBusnummer, ", ", ?code," ", ?adGemeente, ", ", ?adLand) AS ?fullAddress)
-
-  ?sadres a <http://www.w3.org/ns/locn#Address> ;
-         mu:uuid ?sadUuid ;
-         locn:postCode ?sadPostcode ;
-         locn:thoroughfare ?sadStraatnaam ;
-         adres:land ?sadLand ;
-         adres:gemeentenaam ?sadGemeente .
-         OPTIONAL {
-          ?sadres a <http://www.w3.org/ns/locn#Address> ;
-          adres:Adresvoorstelling.busnummer ?sadBusnummer .
+        BIND (CONCAT(?adStraatnaam, " ", ?adHuisnummer, " ", ?adBusnummer, ", ", ?code, " ", ?adGemeente, ", ", ?adLand) AS ?fullAddress)
+      }
+      OPTIONAL {
+        ?site organisatie:bestaatUit ?sadres ;
+              mu:uuid ?Suuid .
+        OPTIONAL {
+          ?site dcterms:description ?sDes .
+        }
+        OPTIONAL {
+          ?site ere:vestigingstype ?sSiteType .
+          OPTIONAL {
+            ?sSiteType a code:TypeVestiging ;
+                       mu:uuid ?sSiteTypeUuid ;
+                       skos:prefLabel ?sSiteTypeName .
           }
-                  OPTIONAL {
+        }
+        OPTIONAL {
           ?sadres a <http://www.w3.org/ns/locn#Address> ;
-          adres:Adresvoorstelling.huisnummer ?sadHuisnummer .
+                  mu:uuid ?sadUuid ;
+                  locn:postCode ?sadPostcode ;
+                  locn:thoroughfare ?sadStraatnaam ;
+                  adres:land ?sadLand ;
+                  adres:gemeentenaam ?sadGemeente .
+          OPTIONAL {
+            ?sadres adres:Adresvoorstelling.busnummer ?sadBusnummer .
           }
-
-BIND(CONCAT(?sadStraatnaam, " ", ?sadHuisnummer, " ", ?sadBusnummer, ", ", ?sadPostcode," ", ?sadGemeente, ", ", ?sadLand) AS ?sadFullAddress)
-
-}}
-
-  BIND(IRI(CONCAT("http://mu.semte.ch/graphs/organizations/", ?adminUnitUuid)) AS ?g)
-
+          OPTIONAL {
+            ?sadres adres:Adresvoorstelling.huisnummer ?sadHuisnummer .
+          }
+          BIND (CONCAT(?sadStraatnaam, " ", ?sadHuisnummer, " ", ?sadBusnummer, ", ", ?sadPostcode, " ", ?sadGemeente, ", ", ?sadLand) AS ?sadFullAddress)
+        }
+      }
+    }
+  }
+  BIND (IRI(CONCAT("http://mu.semte.ch/graphs/organizations/", ?adminUnitUuid)) AS ?g)
 }
      `,
     undefined,
